f6cbc6ca28f0700a0b545ab325b88b756d4cce08
Ghislain Byimbo <byimbog250@gmail.com>
2025-07-31 21:36:08 +0200

    first commit

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const router = require('./routes/tasks.js');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection
+mongoose.connect(`mongodb+srv://byimbog250:jeXs3doILjZn6Cds@cluster0.lvob1gv.mongodb.net/task-manager?retryWrites=true&w=majority&tls=true`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+// Routes
+app.use('/api/tasks', router);
+
+// Start Server
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port: http://localhost:${PORT}`);
+});
